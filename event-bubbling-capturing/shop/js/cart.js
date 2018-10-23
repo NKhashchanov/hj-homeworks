78cc74d4c74091e21099cd44fdb66046ee0e5cd2
Николай <37711307+NKhashchanov@users.noreply.github.com>
2018-10-23 14:05:18 +0300

    up

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,14 @@
 'use strict';
+document.querySelector('.items-list').addEventListener('click', addToCartButton);
+
+function addToCartButton(event) {
+    if (!event.target.classList.contains('add-to-cart')) {
+        return;
+    }
+    event.preventDefault();
+    const title = event.target.dataset.title;
+    const price = event.target.dataset.price;
+    const item = items
+        .filter(item => ( (item.title === title) && (item.price === +price) )).pop();
+    addToCart(item, title, price);
+}
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,21 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    let target = event.target;
+    const table = event.currentTarget;
+
+    if (target.tagName !== 'TH') {
+        return;
+    }
+    if (target.dataset.dir === '1') {
+        target.dataset.dir = '-1'
+    } else {
+        target.dataset.dir = '1'
+    }
+    table.dataset.sortBy = target.dataset.propName;
+    sortTable(target.dataset.propName, Number(target.dataset.dir));
 }
+
+
+const table = document.querySelector('table');
+table.addEventListener('click', handleTableClick);
