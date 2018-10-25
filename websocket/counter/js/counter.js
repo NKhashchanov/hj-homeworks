1abff2ab2cdf0e7284668960d954f044edc6ce94
Николай <37711307+NKhashchanov@users.noreply.github.com>
2018-10-25 11:10:29 +0300

    update

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,8 +1,5 @@
 'use strict';
 const socket = new WebSocket('wss://neto-api.herokuapp.com/counter');
-
-
-
 socket.addEventListener('message', event => {
     try {
         const msg = JSON.parse(event.data);
@@ -10,5 +7,9 @@ socket.addEventListener('message', event => {
         document.querySelector('.errors').value = msg.errors;
     } catch (err) {
         console.log(err);
+    }
+    if (socket.bufferedAmount === 0) {
+        console.log(`socket.bufferedAmount = 0`);
+        socket.close(1000, 'just because');
     }
 });
